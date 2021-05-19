5e8b7f19527569778bb8418545ef023ee8a232ee
Vasiliy16 <83236630+Vasiliy16@users.noreply.github.com>
2021-05-20 00:33:00 +0300

    Add files via upload

---
+++ неделя 1/№1/Неделя 1 №1/Неделя 1 №1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+int main()
+{
+	std::cout << "****** " << std::endl
+		<< "*     *" << std::endl
+		<< "*     *" << std::endl
+		<< "****** " << std::endl
+		<< "*     *" << std::endl
+		<< "*     *" << std::endl
+		<< "****** " << std::endl;
+	std::getchar();
+	std::getchar();
+	return 0;
+}
