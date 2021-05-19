259db6dcc3c2cb024ea0d8b47f581b0d666bb6c7
Vasiliy16 <83236630+Vasiliy16@users.noreply.github.com>
2021-05-20 00:36:19 +0300

    Add files via upload

---
+++ неделя 1/№3/№3/№3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    int a, b;
+    std::cout << "Vvedi pervoe chislo: ";
+    std::cin >>  a;
+    std::cout << "Vvedi vtoroe chislo: ";
+    std::cin >>  b;
+
+    std::cout << b << std::endl;
+    std::cout << a << std::endl;
+  
+    return 0;
+}
