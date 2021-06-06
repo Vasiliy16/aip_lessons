0de89624f76c107461a4eb30238d1ba0d97aba4e
Vasiliy16 <83236630+Vasiliy16@users.noreply.github.com>
2021-06-06 10:02:32 +0300

    Add files via upload

---
+++ неделя 2-3/№2/№2/№2.cpp
@@ -0,0 +1,24 @@
+﻿//Информация(байты, килобайты, мегабайты)
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    float  a;
+
+    std::cout << "Enter the value: ";
+    std::cin >> a;
+    std::cout << a << " Mb = " << a * 1048576 << " b " << std::endl;
+    std::cout << a << " Mb = " << a * 1024 << " Kb  " << std::endl;
+    std::cout << a << " b = " << a / 1048576 << " Mb " << std::endl;
+    std::cout << a << " b = " << a / 1024 << " Kb  " << std::endl;
+    std::cout << a << " Kb  = " << a / 1024 << " Mb " << std::endl;
+    std::cout << a << " Kb  = " << a * 1024 << " b " << std::endl;
+
+    std::getchar();
+    std::getchar();
+
+    return 0;
+}
